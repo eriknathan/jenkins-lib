1725ae6242eeecf908081bcf5f5a9c48bf1ce4ce
erik-nathan <eriknathan.contato@gmail.com>
2023-04-04 13:24:07 -0300

    update no jenkinsfile

--- src/com/example/utils/ConfigApiRest.groovy
+++ src/com/example/utils/ConfigApiRest.groovy
@@ -9,9 +9,10 @@ class ConfigApiRest {
 		//def configBuild(Map params){
 			//if ("${params.ProjectName}" == "apirestul") {
 		if ("apirestful" == "apirestful") {
-			configFileProvider(
-				[configFile(fileId: '9b574e66-ecee-4080-a3b0-890227ab7314', targetLocation: "alerta-discord-pipeline.py")]) {
-			}
+			wrap([$class: 'ConfigFileBuildWrapper', configFileId: '9b574e66-ecee-4080-a3b0-890227ab7314', targetLocation: 'alerta-discord-pipeline.py']) {
+			// configFileProvider(
+			// 	[configFile(fileId: '9b574e66-ecee-4080-a3b0-890227ab7314', targetLocation: "alerta-discord-pipeline.py")]) {
+			// }
 			sh "sudo python3 alerta-discord-pipeline.py"
 		}          
 		//}
@@ -22,9 +23,10 @@ class ConfigApiRest {
 		//def configRun(Map params){
 			//if ("${params.BranchName}"=="main") {
 		if ("main"=="main") {
-			configFileProvider(
-				[configFile(fileId: '9b574e66-ecee-4080-a3b0-890227ab7314', targetLocation: "alerta-discord-pipeline.py")]) {
-			}
+			wrap([$class: 'ConfigFileBuildWrapper', configFileId: '9b574e66-ecee-4080-a3b0-890227ab7314', targetLocation: 'alerta-discord-pipeline.py']) {
+			//configFileProvider(
+			//	[configFile(fileId: '9b574e66-ecee-4080-a3b0-890227ab7314', targetLocation: "alerta-discord-pipeline.py")]) {
+			//}
 			sh "sudo python3 alerta-discord-pipeline.py"
 		}
 		//}
--- vars/apiRestFul.groovy
+++ vars/apiRestFul.groovy
@@ -10,6 +10,15 @@ def call (Map pipelineParams) {
     pipeline {
         agent any
 
+        options {
+        // Adicione a dependência do configFileProvider
+        // Certifique-se de atualizar a versão se necessário
+        // A versão 2.18 é usada como exemplo aqui
+            plugins {
+                configFileProvider('2.18')
+            }
+        }
+
         stages {
             
             stage('Build') {
