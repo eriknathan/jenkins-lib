026a9d3809e43e5fc70ee17c86b729969f49231c
erik-nathan <eriknathan.contato@gmail.com>
2023-04-04 10:20:58 -0300

    atualizando o jenkinsfile

--- vars/apiRestFul.groovy
+++ vars/apiRestFul.groovy
@@ -52,7 +52,7 @@ def call (Map pipelineParams) {
 }
 
 def configBuild(Map params){
-	if ("${params.ProjectName}" == "jenkinslib-multbranch") {
+	if ("${params.ProjectName}" == "apiRestFul") {
 		configFileProvider(
 			[configFile(fileId: '9b574e66-ecee-4080-a3b0-890227ab7314', targetLocation: "alerta-discord-pipeline.py")]) {
 		}
