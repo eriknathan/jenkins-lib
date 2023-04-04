7965561ce175914ee13d55e7303138ef880446f5
erik-nathan <eriknathan.contato@gmail.com>
2023-04-04 12:06:13 -0300

    update no jenkinsfile

--- src/com/example/utils/ConfigApiRest.groovy
+++ src/com/example/utils/ConfigApiRest.groovy
@@ -5,29 +5,29 @@ package com.example.utils
 class ConfigApiRest {
     static void configBuild(Map params) {
         // função configBuild aqui
-		def configBuild(Map params){
+		//def configBuild(Map params){
 			//if ("${params.ProjectName}" == "apirestul") {
-			if ("${apirestful}" == "apirestful") {
-				configFileProvider(
-					[configFile(fileId: '9b574e66-ecee-4080-a3b0-890227ab7314', targetLocation: "alerta-discord-pipeline.py")]) {
-				}
-				sh "sudo python3 alerta-discord-pipeline.py"
-				sh "rm alerta-discord-pipeline.py"
-			}          
-		}
+		if ("${apirestful}" == "apirestful") {
+			configFileProvider(
+				[configFile(fileId: '9b574e66-ecee-4080-a3b0-890227ab7314', targetLocation: "alerta-discord-pipeline.py")]) {
+			}
+			sh "sudo python3 alerta-discord-pipeline.py"
+			sh "rm alerta-discord-pipeline.py"
+		}          
+		//}
     }
 
     static void configRun(Map params) {
         // função configRun aqui
-		def configRun(Map params){
+		//def configRun(Map params){
 			//if ("${params.BranchName}"=="main") {
-			if ("main"=="main") {
-				configFileProvider(
-					[configFile(fileId: '9b574e66-ecee-4080-a3b0-890227ab7314', targetLocation: "alerta-discord-pipeline.py")]) {
-				}
-				sh "sudo python3 alerta-discord-pipeline.py"
-				sh "rm alerta-discord-pipeline.py"
+		if ("main"=="main") {
+			configFileProvider(
+				[configFile(fileId: '9b574e66-ecee-4080-a3b0-890227ab7314', targetLocation: "alerta-discord-pipeline.py")]) {
 			}
+			sh "sudo python3 alerta-discord-pipeline.py"
+			sh "rm alerta-discord-pipeline.py"
 		}
+		//}
     }
 }
