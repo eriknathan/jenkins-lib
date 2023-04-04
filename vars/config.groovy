116500aebf8206e67b3109646b13f5ec5b933359
erik-nathan <eriknathan.contato@gmail.com>
2023-04-04 18:00:54 -0300

    update no jenkinsfile

--- vars/config.groovy
+++ vars/config.groovy
@@ -1,8 +1,7 @@
-def configBuild(Map params) {
-	if ("${params.ProjectName}" == "apirestful") {
-	configFileProvider(
-		[configFile(fileId: '9b574e66-ecee-4080-a3b0-890227ab7314', targetLocation: "alerta-discord-pipeline.py")]) {
-	}
-	sh "sudo python3 alerta-discord-pipeline.py"
-	}          
+def call(Map params = [:]) {
+  def projectName = "${params.ProjectName}"
+  if (projectName == 'apirestful') {
+    configFileProvider([configFile(fileId: '9b574e66-ecee-4080-a3b0-890227ab7314', targetLocation: "alerta-discord-pipeline.py")])
+    sh "sudo python3 alerta-discord-pipeline.py"
+  }
 }
