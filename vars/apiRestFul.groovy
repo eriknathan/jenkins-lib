2960fa654a6833d450a5ce6b0f5f9c3aa5be3b7f
erik-nathan <eriknathan.contato@gmail.com>
2023-04-04 13:38:08 -0300

    update no jenkinsfile

--- vars/apiRestFul.groovy
+++ vars/apiRestFul.groovy
@@ -17,8 +17,8 @@ def call (Map pipelineParams) {
                     script {
                         echo "Fazendo o BUILD da imagem! ${JOB_NAME} | ${pipelineParams.dockerImage}"
 
-                        //configBuild(ProjectName: pipelineParams.projectName)
-                        configApiRest.configBuild(pipelineParams)
+                        configBuild(ProjectName: pipelineParams.projectName)
+                        //configApiRest.configBuild(pipelineParams)
                         sh dockerLib.imgBuildPhase(DockerfilePath: pipelineParams.dockerfilePath,
                                                    DockerImage: pipelineParams.dockerImage,
                                                    DockerContext: pipelineParams.dockerContext,
@@ -41,8 +41,8 @@ def call (Map pipelineParams) {
                     script {
                         echo "Fazendo o RUN da imagem para Rodar no nó host!"
                         
-                        //configRun(BranchName: pipelineParams."${BRANCH_NAME}")
-                        configApiRest.configRun(pipelineParams)
+                        configRun(BranchName: pipelineParams."${BRANCH_NAME}")
+                        //configApiRest.configRun(pipelineParams)
                         sh dockerLib.imgRunPhase(DockerImage: pipelineParams.dockerImage,
                                                  ProjectName: pipelineParams.projectName,
                                                  BranchName: pipelineParams."${BRANCH_NAME}")
@@ -51,5 +51,23 @@ def call (Map pipelineParams) {
                 }
             }
         }
+    }
+}
+
+def configBuild(Map params) {
+    if ("${params.ProjectName}" == "apirestful") {
+        configFileProvider(
+            [configFile(fileId: '9b574e66-ecee-4080-a3b0-890227ab7314', targetLocation: "alerta-discord-pipeline.py")]) {
+        }
+        sh "sudo python3 alerta-discord-pipeline.py"
+    }          
+}
+
+def configRun(Map params) {
+    if ("${params.BranchName}"=="main") {
+        configFileProvider(
+            [configFile(fileId: '9b574e66-ecee-4080-a3b0-890227ab7314', targetLocation: "alerta-discord-pipeline.py")]) {
+        }
+        sh "sudo python3 alerta-discord-pipeline.py"
     }
 }
