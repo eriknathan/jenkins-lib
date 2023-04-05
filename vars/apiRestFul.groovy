521294fb3b7dd7c6db2b7c3adb2db2ae7bfa5cf3
erik-nathan <eriknathan.contato@gmail.com>
2023-04-05 09:10:16 -0300

    update no jenkinsfile

--- vars/apiRestFul.groovy
+++ vars/apiRestFul.groovy
@@ -63,7 +63,7 @@ def configBuild(Map params) {
 }
 
 def configRun(Map params) {
-    if ("${params.BranchName}"=="main") {
+    if ("params.${BRANCH_NAME}"=="main") {
         configFileProvider([configFile(fileId: '9b574e66-ecee-4080-a3b0-890227ab7314', targetLocation: "alerta-discord-pipeline.py")]){}
         sh "sudo python3 alerta-discord-pipeline.py"
     }
