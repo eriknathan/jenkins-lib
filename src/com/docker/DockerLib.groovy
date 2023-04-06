748ab53e87643ab47c43f8972e424b7ac9a09382
erik-nathan <eriknathan.contato@gmail.com>
2023-04-05 21:53:45 -0300

    update no jenkinsfile

--- src/com/docker/DockerLib.groovy
+++ src/com/docker/DockerLib.groovy
@@ -19,14 +19,4 @@ class DockerLib {
         "docker image pull ${params.DockerImage}"
         //"docker-compose -f docker-compose.yml -p ${params.ProjectName}-${params.BranchName} up -d"
     }
-
-    def configBuild(Map params) {
-        if ("${params.ProjectName}" == 'apirestful') {
-            configFileProvider(
-                [configFile(fileId: '9b574e66-ecee-4080-a3b0-890227ab7314', targetLocation: "alerta-discord-pipeline.py")]){
-                sh "sudo python3 alerta-discord-pipeline.py"
-            }
-        }
-    }
-
 }
