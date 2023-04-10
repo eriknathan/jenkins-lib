bfae1e9afa73504b98d3603d8c03b510d7cfb07d
erik-nathan <eriknathan.contato@gmail.com>
2023-04-10 13:04:00 -0300

    atualizando o arquivo do apiRestFul

--- vars/apiRestFul.groovy
+++ vars/apiRestFul.groovy
@@ -3,7 +3,6 @@
 def call (Map pipelineParams) {
     
     def dockerLib = new com.docker.DockerLib()
-    def apiRestFul = new com.projetos.ApiRestFul()
 
     pipelineParams.dockerImage = "${DOCKER_REGISTRY}/${pipelineParams.projectName}:${BRANCH_NAME}-${BUILD_NUMBER}"
     pipelineParams.dockerfilePath = "Dockerfile"
@@ -19,7 +18,7 @@ def call (Map pipelineParams) {
                     script {
                         echo "Fazendo o BUILD da imagem! ${JOB_NAME} | ${pipelineParams.dockerImage}"
 
-                        apiRestFul.build(ProjectName: pipelineParams.projectName)                        
+                        configBuild(ProjectName: pipelineParams.projectName)                        
 
                         sh dockerLib.imgBuildPhase(DockerfilePath: pipelineParams.dockerfilePath,
                                                    DockerImage: pipelineParams.dockerImage,
@@ -43,7 +42,7 @@ def call (Map pipelineParams) {
                     script {
                         echo "Fazendo o RUN da imagem para Rodar no nó host!"
                         
-                        apiRestFul.run(BranchName: pipelineParams."${BRANCH_NAME}")
+                        configRun(BranchName: pipelineParams."${BRANCH_NAME}")
                         
                         sh dockerLib.imgRunPhase(DockerImage: pipelineParams.dockerImage,
                                                  ProjectName: pipelineParams.projectName,
@@ -55,17 +54,21 @@ def call (Map pipelineParams) {
         }
     }
 }
-
-def configBuild = {
-    configFileProvider(
-        [configFile(fileId: '9b574e66-ecee-4080-a3b0-890227ab7314', targetLocation: "alerta-discord-pipeline.py")]) {
-        sh "sudo python3 alerta-discord-pipeline.py"
+// criar uma função apenas com o configFileProvider, e criar um arquivo separado para essa pipeline de configuções e chamar a função com o configFile... nele.
+def configBuild(Map params) {
+    if ("${params.ProjectName}" == 'apirestful') {
+        configFileProvider(
+            [configFile(fileId: '9b574e66-ecee-4080-a3b0-890227ab7314', targetLocation: "alerta-discord-pipeline.py")]) {
+            sh "sudo python3 alerta-discord-pipeline.py"
+        }
     }
 }
 
-def configRun = {
-    configFileProvider(
-        [configFile(fileId: '9b574e66-ecee-4080-a3b0-890227ab7314', targetLocation: "alerta-discord-pipeline.py")]) {
-        sh "sudo python3 alerta-discord-pipeline.py"
+def configRun(Map params) {
+    if ("${BRANCH_NAME}"=="main") {
+        configFileProvider(
+            [configFile(fileId: '9b574e66-ecee-4080-a3b0-890227ab7314', targetLocation: "alerta-discord-pipeline.py")]) {
+            sh "sudo python3 alerta-discord-pipeline.py"
+        }
     }
 }
