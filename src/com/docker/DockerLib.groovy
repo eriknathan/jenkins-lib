c6cb0d3a80d9bdf0566a734cd3dab9885a245f19
erik-nathan <eriknathan.contato@gmail.com>
2023-05-02 20:25:53 -0300

    feat: adicioanando a pipeline Delta Reboque

--- src/com/docker/DockerLib.groovy
+++ src/com/docker/DockerLib.groovy
@@ -17,7 +17,7 @@ class DockerLib {
         "echo CONTAINER_NAME=${params.ProjectName}-${params.BranchName} >> .env"
         
         "docker image pull ${params.DockerImage}"
-        //"docker-compose -f docker-compose.yml -p ${params.ProjectName}-${params.BranchName} up -d"
+        "docker-compose -f docker-compose.yml -p ${params.ProjectName}-${params.BranchName} up -d"
     }
 
     def imgPullPhase(Map params){
---
+++ vars/deltaReboque.groovy
@@ -0,0 +1,54 @@
+// vars/apiRestFul.groovy
+
+def call (Map pipelineParams) {
+    
+    def dockerLib = new com.docker.DockerLib()
+
+    pipelineParams.dockerImage = "${DOCKER_REGISTRY}/${pipelineParams.projectName}:${BRANCH_NAME}-${BUILD_NUMBER}"
+    pipelineParams.dockerfilePath = "Dockerfile"
+    pipelineParams.dockerContext = "."
+    
+    pipeline {
+        agent any
+
+        stages {
+            
+            stage('Build') {
+                steps {
+                    script {
+                        echo "Fazendo o BUILD da imagem! ${JOB_NAME} | ${pipelineParams.dockerImage}"
+
+                        sh configBuild(ProjectName: pipelineParams."${BRANCH_NAME}")                        
+
+                        sh dockerLib.imgBuildPhase(DockerfilePath: pipelineParams.dockerfilePath,
+                                                   DockerImage: pipelineParams.dockerImage,
+                                                   DockerContext: pipelineParams.dockerContext,
+                                                   ProjectName: pipelineParams.projectName)
+                    }
+                }
+            }
+
+            stage('Push') {
+                steps {
+                    script {
+                        echo "Fazendo o PUSH da imagem!"
+                        sh dockerLib.imgPushPhase(DockerImage: pipelineParams.dockerImage)
+                    }
+                }
+            }
+
+            stage('Run') {
+                steps {
+                    script {
+                        echo "Fazendo o RUN da imagem para Rodar no nó host!"
+                                                
+                        sh dockerLib.imgRunPhase(DockerImage: pipelineParams.dockerImage,
+                                                 ProjectName: pipelineParams.projectName,
+                                                 BranchName: pipelineParams."${BRANCH_NAME}")
+                    }
+                    deleteDir()
+                }
+            }
+        }
+    }
+}
