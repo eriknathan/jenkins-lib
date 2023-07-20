fc5510a71471ac6995893ab16f92de21baa7a84e
eriknathan <eriknathan.contato@gmail.com>
2023-07-20 09:53:21 -0300

    feat: atualizando o jenkins_lib para testes locais

--- src/com/docker/DockerLib.groovy
+++ src/com/docker/DockerLib.groovy
@@ -5,8 +5,7 @@ package com.docker
 class DockerLib {
 
     def imgBuildPhase(Map params){
-        "echo Teste: ${params.DockerImage}",
-        "docker compose --help"
+        "echo Teste: ${params.DockerImage}; docker compose --help"
         //"docker build -t ${params.DockerImage} --no-cache -f ${params.DockerfilePath} ${params.DockerContext}"
     }
 
