22c2574385550e4f63e7a45de8b44f9f493790a9
eriknathan <eriknathan.contato@gmail.com>
2023-07-21 09:03:56 -0300

    feat: adicionando a função global do Trivy

--- src/com/trivy/TrivyLib.groovy
+++ src/com/trivy/TrivyLib.groovy
@@ -6,6 +6,7 @@ class TrivyLib {
 
     def trivyScanning(Map params){
         ("echo Realizando o Sacanning da Imagem: ${params.DockerImage};" +
-         "trivy image ${params.DockerImage}; echo Exit Code: $?")
+         "trivy image ${params.DockerImage};" +
+		 "echo $?")
     }
 }
