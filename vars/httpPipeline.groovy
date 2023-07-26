1883bae895691e01ba39cc88ad5fe08267caa25f
eriknathan <eriknathan.contato@gmail.com>
2023-07-26 10:18:09 -0300

    feat: adicionando a teste do http

--- vars/httpPipeline.groovy
+++ vars/httpPipeline.groovy
@@ -15,12 +15,26 @@ def call (Map pipelineParams) {
             stage('Helth Check') {
                 steps {
 					script {	
-						echo " --------------------------------------------------------------------------------------- "
-                        echo " HEALTH CHECK"
-                        echo " --------------------------------------------------------------------------------------- "					
-						// Chamando a função de health check
-						httpLib.performHealthCheck(ServiceName: pipelineParams.serviceName,
-										   		   ServiceURL: pipelineParams.serviceURL)
+						// echo " --------------------------------------------------------------------------------------- "
+                        // echo " HEALTH CHECK"
+                        // echo " --------------------------------------------------------------------------------------- "					
+						// // Chamando a função de health check
+						// httpLib.performHealthCheck(ServiceName: pipelineParams.serviceName,
+						// 				   		   ServiceURL: pipelineParams.serviceURL)
+						def serviceName = 'Google' // Substitua pelo nome do serviço que você deseja verificar
+						def serviceURL = 'www.google.com' // Substitua pela URL do serviço
+						
+						def response = httpRequest(
+							url: serviceURL,
+							validResponseCodes: '200', // Códigos de resposta válidos que indicam que o serviço está funcionando
+							validResponseContent: serviceName // Conteúdo esperado na resposta para verificar se é o serviço correto
+						)
+						
+						if (response.status == 200) {
+							echo "O serviço ${serviceName} está funcionando corretamente."
+						} else {
+							error "O serviço ${serviceName} está com problemas. Código de resposta: ${response.status}"
+						}
 					}
 				}
             }
