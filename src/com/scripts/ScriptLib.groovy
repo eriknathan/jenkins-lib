6fc2e6e169c2a561dcb5321c2aaaa6e2021330a9
eriknathan <eriknathan.contato@gmail.com>
2023-08-02 09:11:00 -0300

    feat: organizando arquivos e criando um script para teste na pipeline

--- src/com/scripts/ScriptLib.groovy
+++ src/com/scripts/ScriptLib.groovy
@@ -5,10 +5,7 @@ package com.scripts
 class ScriptLib {
 
     def scritpsSh(Map params){
-		NOME="${params.Name1}"
-		NOME_2="${params.Name2}"
-
-		[ "$NOME" = "$NOME_2" ] && echo "Nomes são Iguais!"
+		'resource/segredos.sh'
     }
 }
 
