6cc734cde80833e2deb85c404f336cd536101948
eriknathan <eriknathan.contato@gmail.com>
2023-08-03 11:55:55 -0300

    script

--- src/com/scripts/ScriptLib.groovy
+++ src/com/scripts/ScriptLib.groovy
@@ -7,7 +7,7 @@ class ScriptLib {
     def testScript(Map params) {
 		def scriptbash = libraryResource "com/scripts/${params.Script}.sh"
 		"echo ${params.Script} ${params.Nome} ${params.Day}"
-		writeFile file: ./"${params.Script}".sh, text: scriptbash
+		writeFile file: "./${params.Script}.sh", text: scriptbash
 		"bash ./${params.Script}.sh ${params.Nome} ${params.Day}"
 	}
 }
