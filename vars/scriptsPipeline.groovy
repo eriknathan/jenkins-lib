230efa78a8d3a393a67f23735bf41226235488b6
eriknathan <eriknathan.contato@gmail.com>
2023-08-03 10:09:59 -0300

    feat: puxando script de uma pasta

--- vars/scriptsPipeline.groovy
+++ vars/scriptsPipeline.groovy
@@ -16,9 +16,21 @@ def call (Map pipelineParams) {
 						echo " INICIANDO O TESTE DO SCRIPT SH "
 						echo " --------------------------------------------------------------------------------------- "
 					}
-					def script_bash = libraryResource 'resource/scripts/segredos.sh'
-					writeFile file: './segredos.sh', text: script_bash
-					sh 'bash ./segredos.sh'
+					// def script_bash = libraryResource 'resource/scripts/segredos.sh'
+					// writeFile file: './segredos.sh', text: script_bash
+					// sh 'bash ./segredos.sh'
+
+					def my_function(string serverip, string scriptargument) {
+						def script_content = libraryresource 'my_scripts/test.sh'
+						// create a file with script_bash content
+						writefile file: './test.sh', text: script_content
+						echo "execute remote script test.sh..."
+						def sshcommand = "ssh username@${serverip} \'bash -xs\' < ./test.sh ${scriptargument}"
+						echo "ssh command is: ${sshcommand}"
+						sh(sshcommand)
+					}
+
+					my_script.my_function("192.168.1.1", "scriptargumentvalue")
 				}
 			}
 		}
