b8f460a742d8efe4def1a44bbb9e048e86a7d5e0
eriknathan <eriknathan.contato@gmail.com>
2023-08-03 11:53:51 -0300

    script

--- src/com/scripts/ScriptLib.groovy
+++ src/com/scripts/ScriptLib.groovy
@@ -5,6 +5,8 @@ package com.scripts
 class ScriptLib {
 
     def testScript(Map params) {
+		def scriptbash = libraryResource "com/scripts/${params.Script}.sh"
+		writeFile file: ./"${params.Script}".sh, text: scriptbash
 		"bash ./${params.Script}.sh ${params.Nome} ${params.Day}"
 	}
 }
--- vars/scriptsPipeline.groovy
+++ vars/scriptsPipeline.groovy
@@ -19,8 +19,8 @@ def call (Map pipelineParams) {
 						echo " INICIANDO O TESTE DO SCRIPT SH "
 						echo " --------------------------------------------------------------------------------------- "
 						
-						def scriptbash = libraryResource 'com/scripts/segredos.sh'
-						writeFile file: './segredos.sh', text: scriptbash
+						//def scriptbash = libraryResource 'com/scripts/segredos.sh'
+						//writeFile file: './segredos.sh', text: scriptbash
 						//sh 'bash ./segredos.sh Erik Quinta'
 
 						sh scriptLib.testScript(Script:"segredos", Nome:"Erik", Day:"Quinta")
