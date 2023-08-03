7e3610b7e4050f0cc80b248fec3a009e92aba6c9
eriknathan <eriknathan.contato@gmail.com>
2023-08-03 10:58:24 -0300

    feat: puxando script de uma pasta

--- vars/scriptsPipeline.groovy
+++ vars/scriptsPipeline.groovy
@@ -16,7 +16,7 @@ def call (Map pipelineParams) {
 						echo " INICIANDO O TESTE DO SCRIPT SH "
 						echo " --------------------------------------------------------------------------------------- "
 					}
-            		def script_bash = libraryResource 'com/example/loadtestscript'
+            		def script_bash = libraryResource 'resource/com/scripts'
 				}
 			}
 		}
