9580b8d2a46fce8fbde7ffb79b05bde417740c94
eriknathan <eriknathan.contato@gmail.com>
2023-08-03 13:37:12 -0300

    script

--- vars/scriptsPipeline.groovy
+++ vars/scriptsPipeline.groovy
@@ -5,9 +5,6 @@ def call (Map pipelineParams) {
 	def scriptLib = new com.scripts.ScriptLib()
 	def cleanLib = new com.functions.CleanLib()
 
-    pipelineParams.scriptShell = "segredos.sh"
-    pipelineParams.scriptPython = "teste.py"
-    
 
     pipeline {
         agent { 
@@ -24,17 +21,17 @@ def call (Map pipelineParams) {
 						echo " INICIANDO O TESTE DO SCRIPT SH "
 						echo " --------------------------------------------------------------------------------------- "
 						
-						def scriptbash = libraryResource "com/scripts/${pipelineParams.scriptShell}"
-						writeFile file: "./${pipelineParams.scriptShell}", text: scriptbash
-						sh "bash ./${pipelineParams.scriptShell}.sh Erik 21 Masc"
+						def scriptbash = libraryResource 'com/scripts/segredos.sh'
+						writeFile file: './segredos.sh', text: scriptbash
+						sh 'bash ./segredos.sh Erik 21 Masc'
 
-						sh cleanLib.cleanFiles(File: pipelineParams.scriptShell)
+						sh cleanLib.cleanfile(File:"segredos.sh")
 
-						def scriptpython = libraryResource "com/scripts/${pipelineParams.scriptPython}"
-						writeFile file: "./${pipelineParams.scriptPython}", text: scriptpython
-						sh "python3 ./${pipelineParams.scriptPython} Erik"
+						def scriptpython = libraryResource 'com/scripts/teste.py'
+						writeFile file: './teste.py', text: scriptpython
+						sh 'python3 ./teste.py Erik'
 						
-						sh cleanLib.cleanFiles(File: pipelineParams.scriptPython)
+						sh cleanLib.cleanfile(File:"teste.py")
 					}
 				}
 			}
