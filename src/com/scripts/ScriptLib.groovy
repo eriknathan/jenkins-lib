3e7b9b6bb475ae5c524cdc263d3c6b3a2166fc2e
eriknathan <eriknathan.contato@gmail.com>
2023-08-03 10:12:57 -0300

    feat: puxando script de uma pasta

--- src/com/scripts/ScriptLib.groovy
+++ src/com/scripts/ScriptLib.groovy
@@ -4,12 +4,15 @@ package com.scripts
 
 class ScriptLib {
 
-    def scritpsSh(){
-		("echo Erik Nathan de Oliveira Batista > segredo.sh;" +
-		 "chmod +x segredo.sh;" +
-		 "./segredo.sh")
-
-    }
+    def my_function(string serverip, string scriptargument) {
+		def script_content = libraryresource 'my_scripts/test.sh'
+		// create a file with script_bash content
+		writefile file: './test.sh', text: script_content
+		echo "execute remote script test.sh..."
+		def sshcommand = "ssh username@${serverip} \'bash -xs\' < ./test.sh ${scriptargument}"
+		echo "ssh command is: ${sshcommand}"
+		sh(sshcommand)
+	}
 }
 
 
--- vars/scriptsPipeline.groovy
+++ vars/scriptsPipeline.groovy
@@ -2,6 +2,8 @@
 
 def call (Map pipelineParams) {
     
+	def my_script = com.scripts.ScriptLib
+
     pipeline {
         agent { 
             label 'ubuntu'
@@ -14,17 +16,6 @@ def call (Map pipelineParams) {
 					// def script_bash = libraryResource 'resource/scripts/segredos.sh'
 					// writeFile file: './segredos.sh', text: script_bash
 					// sh 'bash ./segredos.sh'
-
-					def my_function(string serverip, string scriptargument) {
-						def script_content = libraryresource 'my_scripts/test.sh'
-						// create a file with script_bash content
-						writefile file: './test.sh', text: script_content
-						echo "execute remote script test.sh..."
-						def sshcommand = "ssh username@${serverip} \'bash -xs\' < ./test.sh ${scriptargument}"
-						echo "ssh command is: ${sshcommand}"
-						sh(sshcommand)
-					}
-
                     script {
                         echo " --------------------------------------------------------------------------------------- "
 						echo " INICIANDO O TESTE DO SCRIPT SH "
